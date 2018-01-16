6a4fc528b9d0ca85dba19875887341fb9822268f
Fabrice Sommavilla <flasomm@gmail.com>
2018-01-16 18:38:47 +0100

    fix clock

--- clock/AnalogClock.py
+++ clock/AnalogClock.py
@@ -10,19 +10,38 @@ Qt and PyQt.
 
 class AnalogClock(QtWidgets.QWidget):
 
-    def __init__(self, parent=None):
-        super(AnalogClock, self).__init__(parent)
+    def __init__(self):
+        super().__init__()
 
         timer = QtCore.QTimer(self)
         timer.timeout.connect(self.update)
         timer.start(1000)
 
-        self.color = QtGui.QColor(205, 216, 87)
+        self.color = QtGui.QColor(113, 249, 76)
 
         self.setAutoFillBackground(True)
         palette = self.palette()
         palette.setColor(self.backgroundRole(), QtCore.Qt.black)
         self.setPalette(palette)
+
+        self.hourHand = QtGui.QPolygon([
+            QtCore.QPoint(0, 0),
+            QtCore.QPoint(0, -40)
+        ])
+        self.minuteHand = QtGui.QPolygon([
+            QtCore.QPoint(0, 0),
+            QtCore.QPoint(0, -70)
+        ])
+        self.secondHand = QtGui.QPolygon([
+            QtCore.QPoint(0, 0),
+            QtCore.QPoint(0, -80)
+        ])
+
+    def paint_needle(self, painter, angle, needle):
+        painter.save()
+        painter.rotate(angle)
+        painter.drawConvexPolygon(needle)
+        painter.restore()
 
     def paintEvent(self, event):
         side = min(self.width(), self.height())
@@ -36,35 +55,26 @@ class AnalogClock(QtWidgets.QWidget):
 
         pen = QtGui.QPen()
         pen.setStyle(QtCore.Qt.SolidLine)
-        pen.setWidth(4)
+        pen.setWidth(2)
         pen.setCapStyle(QtCore.Qt.RoundCap)
         pen.setColor(self.color)
         painter.setPen(pen)
 
-        painter.save()
-        painter.rotate(30.0 * (time.hour() + time.minute() / 60.0))
-        painter.drawLine(0, 0, -35, -35)
-        painter.restore()
+        self.paint_needle(painter, 30.0 * (time.hour() + time.minute() / 60.0), self.hourHand)
 
         for i in range(0, 12):
             painter.drawLine(92, 0, 96, 0)
             painter.rotate(30.0)
 
-        painter.save()
-        painter.rotate(6.0 * (time.minute() + time.second() / 60.0))
-        painter.drawLine(0, 0, -50, -50)
-        painter.restore()
+        self.paint_needle(painter, 6.0 * (time.minute() + time.second() / 60.0), self.minuteHand)
 
         pen.setWidth(1)
         painter.setPen(pen)
 
-        painter.save()
-        painter.rotate(6.0 * time.second())
-        painter.drawLine(0, 0, -60, -60)
-        painter.restore()
+        self.paint_needle(painter, 6.0 * time.second(), self.secondHand)
 
         for j in range(0, 60):
-            if (j % 5) != 6:
+            if (j % 5) != 0:
                 painter.drawLine(94, 0, 96, 0)
             painter.rotate(6.0)
 
